c256107578dfa01874dcd4286f764a62e64c5ca3
goro9 <sokenbiccha@icloud.com>
2021-01-17 14:50:58 +0900

    remove unnecessary directory in examples

--- cmd/korat/main.go
+++ cmd/korat/main.go
@@ -5,7 +5,8 @@ import (
 )
 
 func main() {
+	// TODO: set from command line option by cobra
 	adapterID := "hci0"
-	args := []string{"service", "server"}
+	args := []string{"server", ""}
 	service_example.Run(adapterID, args[0], args[1])
 }
